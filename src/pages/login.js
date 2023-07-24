4b5311343294250721965f89caa06894c0dd5ddc
zoodaa <jkj01230@gmail.com>
2023-07-24 20:18:37 +0900

    fix: 불필요한 import 문 삭제

--- src/pages/login.js
+++ src/pages/login.js
@@ -2,9 +2,6 @@ import NavBar from "@/components/common/navbar/NavBar";
 import PageHead from "@/components/common/page-head/PageHead";
 import LoginLeftTab from "@/components/pages/login/login-left-tab/LoginLeftTab";
 import LoginRightTab from "@/components/pages/login/login-right-tab/LoginRightTab";
-import LoginInputForm from "@/components/pages/login/login-input-form/LoginInputForm";
-import FindLinkBox from "@/components/pages/login/find-link-box/FindLinkBox";
-import SnsLoginBox from "@/components/pages/login/sns-login-box/SnsLoginBox";
 
 export default function login() {
   return (
@@ -13,7 +10,6 @@ export default function login() {
       <NavBar />
         <LoginLeftTab />
         <LoginRightTab />
-
     </div>
   );
 }
