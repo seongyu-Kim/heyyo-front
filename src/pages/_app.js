198ebe1e5afc28af4a74eb223911a187618e5c1d
seongyu <seongyu1066@naver.com>
2023-07-28 15:06:25 +0900

    feat: 공통으로 사용할 class인 container를 app.js에 추가

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,19 @@
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+      <>
+        <Component {...pageProps} />
+        <style jsx global>{`
+            .container {
+              display: flex;
+              flex-wrap: wrap;
+              justify-content: center;
+              text-align: center;
+              position: relative;
+            }
+          `}
+        </style>
+      </>
+  )
 }
