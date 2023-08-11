0612591241746610616379a2e36230eb57d644df
zoodaa <jkj01230@gmail.com>
2023-08-11 20:35:01 +0900

    feat: Swiper.js로 회원가입 페이지 구현 및 관련 페이지 추가
    Swiper.js를 활용하여 회원가입 페이지를 구현하고,필요한 추가 페이지 추가

--- src/components/pages/signup/signup-right-tab/SignupRightTab.js
+++ src/components/pages/signup/signup-right-tab/SignupRightTab.js
@@ -1,42 +1,104 @@
-import SignupInputForm from "@/components/pages/signup/signup-input-form/SignupInputForm";
+import React, { useRef, useState } from "react";
+import SwiperCore, { Virtual } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SignupPageContent from "@/components/pages/signup/signup-page-content/SignupPageContent";
+import SignupRecommenderForm from "@/components/pages/signup/signup-recommender-form/SignupRecommenderForm";
+import { Pagination, Navigation } from "swiper/modules";
+import Image from "next/image";
+
+SwiperCore.use([Virtual, Navigation, Pagination]);
 
 export default function SignupRightTab() {
-    return(
-        <div className="sign-right">
-            <span className="sign-right-text">어서오세요!<br/>해요에 여러분을 입력하세요.</span>
-            <SignupInputForm />
-            {/* CSS */}
-            <style jsx>{`
-            .sign-right {
-              position: absolute;
-              width: 41.5rem;
-              height: 37.4375rem;
-              flex-shrink: 0;
-              background: #FFF;
-              box-shadow: 4px 4px 30px 2px rgba(0, 0, 0, 0.10);
-              left: 48.6rem;
-              right: 29.9rem;
-              top: 17.8rem;
-              bottom: 12.3rem;
-              display: flex;
-              flex-direction: column;
-              text-align: left;
-            }
-            
-            .sign-right-text {
-              display: block;
-              color: #000;
-              font-family: Noto Sans KR;
-              font-size: 2.25rem;
-              font-style: normal;
-              font-weight: 700;
-              line-height: 129.8%;
-              padding-left: 7.37rem;
-              padding-top: 5.12rem;
-              padding-bottom: 2.81rem;
-            }
-        `}
-            </style>
+  const swiperRef = useRef();
+  const [isSignupPageContent, setIsSignupPageContent] = useState(true);
+
+  const handleSlideChange = () => {
+    setIsSignupPageContent(!isSignupPageContent);
+  };
+
+  return (
+    <div className="sign-right-tab">
+      {isSignupPageContent && (
+        <div className="scroll-down-button">
+          <Image
+            src="/icon/ScrollDownButton.svg"
+            alt="scrollupbutton"
+            loader={() => "/icon/ScrollDownButton.svg"}
+            width={24}
+            height={24}
+            onClick={() => swiperRef.current?.slideNext()}
+          />
         </div>
-    );
+      )}
+      {!isSignupPageContent && (
+        <div className="scroll-up-button">
+          <Image
+            src="/icon/ScrollUpButton.svg"
+            alt="scrollupbutton"
+            loader={() => "/icon/ScrollUpButton.svg"}
+            width={24}
+            height={24}
+            onClick={() => swiperRef.current?.slidePrev()}
+          />
+        </div>
+      )}
+      <Swiper
+        style={{ width: "100%", height: "100%" }}
+        direction="vertical"
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        onSlideChange={handleSlideChange}
+        onBeforeInit={(swiper) => {
+          swiperRef.current = swiper;
+        }}
+      >
+        <SwiperSlide>
+          <SignupPageContent />
+        </SwiperSlide>
+        <SwiperSlide>
+          <SignupRecommenderForm />
+        </SwiperSlide>
+      </Swiper>
+
+      {/* CSS */}
+      <style jsx>
+        {`
+          .sign-right-tab {
+            position: absolute;
+            overflow: hidden;
+            width: 41.5rem;
+            height: 37.4375rem;
+            background: #fff;
+            box-shadow: 4px 4px 30px 2px rgba(0, 0, 0, 0.1);
+            left: 48.63rem;
+            right: 29.88rem;
+            top: 17.81rem;
+            bottom: 12.25rem;
+          }
+
+          .scroll-up-button {
+            position: absolute;
+            max-width: 100%;
+            width: 62.5rem;
+            height: 1.5rem;
+            left: 50%;
+            top: 2.52rem;
+            z-index: 100;
+          }
+          .scroll-down-button {
+            position: absolute;
+            max-width: 100%;
+            width: 62.5rem;
+            height: 1.5rem;
+            left: 50%;
+            bottom: 1.02rem;
+            z-index: 100;
+          }
+        `}
+      </style>
+    </div>
+  );
 }
