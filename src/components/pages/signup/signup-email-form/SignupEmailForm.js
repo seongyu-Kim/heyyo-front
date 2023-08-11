06c0be6f2b43d3bd6cd3e61cf235df87274d4362
zoodaa <jkj01230@gmail.com>
2023-08-11 20:23:49 +0900

    feat: 이메일 컴포넌트 구현

--- src/components/pages/signup/signup-email-form/SignupEmailForm.js
+++ src/components/pages/signup/signup-email-form/SignupEmailForm.js
@@ -1,5 +1,47 @@
+import { Font } from "/public/fonts/Font";
+
 export default function SignupEmailForm() {
-    return(
-        <div></div>
-    );
+  return (
+    <div className="box">
+      <div className="email-container">
+        <input
+          name="email"
+          className="email-input"
+          type="text"
+          maxLength="15"
+          placeholder="이메일을 입력해주세요."
+        />
+      </div>
+
+      {/* CSS */}
+      <style jsx>
+        {`
+          .email-container {
+            display: flex;
+            align-items: center;
+            margin-left: 7.37rem;
+            margin-bottom: 0.94rem;
+          }
+
+          .email-input {
+            width: 18.375rem;
+            height: 2.125rem;
+            padding: 18px;
+            border: none;
+            border-radius: 0.5625rem;
+            background: #e9f7ff;
+
+            /* Placeholder 스타일 지정 */
+            ::placeholder {
+              color: #000;
+              text-align: center;
+              font-size: ${Font.Size.S};
+              font-weight: 400;
+              line-height: 129.8%;
+            }
+          }
+        `}
+      </style>
+    </div>
+  );
 }
