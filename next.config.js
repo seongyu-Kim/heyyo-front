40df58538086f9b0e8a76121f239e77ff185be93
zoodaa <jkj01230@gmail.com>
2023-08-19 13:24:10 +0900

    feat: API 경로 리다이렉션 설정 추가
    CORS 에러 해결을 위해 next.config.js 에 API 경로 리다이렉션 설정 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  // swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/users/register",
+        destination: "http://3.35.176.114:8080/api/users/register",
+      },
+      {
+        source: "/api/users/duplicate/nicknames/:nickname",
+        destination:
+          "http://3.35.176.114:8080/api/users/duplicate/nicknames/:nickname",
+      },
+      {
+        source: "/api/users/duplicate/emails/:email",
+        destination:
+          "http://3.35.176.114:8080/api/users/duplicate/emails/:email",
+      },
+      {
+        source: "/api/users/sms/:phoneNumber",
+        destination: "http://3.35.176.114:8080/api/users/sms/:phoneNumber",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
