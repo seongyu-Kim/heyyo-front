bb51d5f4736c4edea272f9a208985f10b0600081
zoodaa <jkj01230@gmail.com>
2023-08-19 13:18:01 +0900

    feat: Recoil Atom 상태 정의 추가
    Atom 상태를 정의하고 초기값을 설정

---
+++ src/recoil/atoms/Atoms.js
@@ -0,0 +1,31 @@
+import { atom } from "recoil";
+
+export const nameState = atom({
+  key: "nameState",
+  default: "",
+});
+
+export const nicknameState = atom({
+  key: "nicknameState",
+  default: "",
+});
+
+export const passwordState = atom({
+  key: "passwordState",
+  default: "",
+});
+
+export const emailState = atom({
+  key: "emailState",
+  default: "",
+});
+
+export const phoneNumberState = atom({
+  key: "phoneNumberState",
+  default: "",
+});
+
+export const recommendNicknameState = atom({
+  key: "recommendNicknameState",
+  default: "",
+});
