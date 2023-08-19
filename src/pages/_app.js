8388f4b32139b8e1c99198c2ad0f0374ea8eeebc
zoodaa <jkj01230@gmail.com>
2023-08-19 13:36:04 +0900

    feat: RecoilRoot 를 _app.js 에 추가
    Recoil 의 상태 관리를 위해 RecoilRoot 추가

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,27 +2,30 @@ import "@/styles/globals.css";
 import { fontStyles } from "/public/fonts/Font";
 import PageHead from "@/components/common/page-head/PageHead";
 import NavBar from "@/components/common/navbar/NavBar";
+import { RecoilRoot } from "recoil";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <style jsx global>
-        {fontStyles}
-      </style>
-      <PageHead />
-      <NavBar />
-      <Component {...pageProps} />
-      <style jsx global>
-        {`
-          .container {
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: center;
-            text-align: center;
-            position: relative;
-          }
-        `}
-      </style>
+      <RecoilRoot>
+        <style jsx global>
+          {fontStyles}
+        </style>
+        <PageHead />
+        <NavBar />
+        <Component {...pageProps} />
+        <style jsx global>
+          {`
+            .container {
+              display: flex;
+              flex-wrap: wrap;
+              justify-content: center;
+              text-align: center;
+              position: relative;
+            }
+          `}
+        </style>
+      </RecoilRoot>
     </>
   );
 }
