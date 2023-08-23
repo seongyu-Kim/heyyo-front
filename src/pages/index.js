85bc76bd84114991199bf4e7962ae5a3692468f7
zoodaa <jkj01230@gmail.com>
2023-08-23 22:41:08 +0900

    refactor: 불필요한 import 문 삭제

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,9 @@
-import NavBar from "@/components/common/navbar/NavBar";
-import PageHead from "@/components/common/page-head/PageHead";
-
 export default function Home() {
   return (
-      <>
-        <div>
-          <h1>Home</h1>
-        </div>
-      </>
+    <>
+      <div>
+        <h1>Home</h1>
+      </div>
+    </>
   );
 }
