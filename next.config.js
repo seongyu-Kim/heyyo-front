d8d442a7278655173ee5fd85ce3f3fbe1d37d507
zoodaa <jkj01230@gmail.com>
2023-08-25 20:37:33 +0900

    refactor: 서버 ip 변경으로 인해 ip 수정

--- next.config.js
+++ next.config.js
@@ -6,29 +6,29 @@ const nextConfig = {
     return [
       {
         source: "/api/users/register",
-        destination: "http://3.35.176.114:8080/api/users/register",
+        destination: "http://15.165.120.144:8080/api/users/register",
       },
       {
         source: "/api/users/duplicate/nicknames/:nickname",
         destination:
-          "http://3.35.176.114:8080/api/users/duplicate/nicknames/:nickname",
+          "http://15.165.120.144:8080/api/users/duplicate/nicknames/:nickname",
       },
       {
         source: "/api/users/duplicate/emails/:email",
         destination:
-          "http://3.35.176.114:8080/api/users/duplicate/emails/:email",
+          "http://15.165.120.144:8080/api/users/duplicate/emails/:email",
       },
       {
         source: "/api/users/sms/:phoneNumber",
-        destination: "http://3.35.176.114:8080/api/users/sms/:phoneNumber",
+        destination: "http://15.165.120.144:8080/api/users/sms/:phoneNumber",
       },
       {
         source: "/api/users/login",
-        destination: "http://3.35.176.114:8080/api/users/login",
+        destination: "http://15.165.120.144:8080/api/users/login",
       },
       {
         source: "/api/logout",
-        destination: "http://3.35.176.114:8080/api/logout",
+        destination: "http://15.165.120.144:8080/api/logout",
       },
     ];
   },
