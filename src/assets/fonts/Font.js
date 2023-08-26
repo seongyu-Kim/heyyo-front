56fda8d7f3f531d3ea1a5ddda3acf8e234841687
zoodaa <jkj01230@gmail.com>
2023-08-26 16:12:39 +0900

    refactor: 폰트 사이즈 수정
    px -> rem 으로 변경

--- src/assets/fonts/Font.js
+++ src/assets/fonts/Font.js
@@ -1,17 +1,11 @@
 export const Font = {
   Family: "Noto Sans KR",
   Size: {
-    // XS: 0.625rem
-    XS: "10px",
-    // S: 0.8125rem
-    S: "13px",
-    // M: 1.125rem
-    M: "18px",
-    // L: 1.5rem
-    L: "24px",
-    // XL: 2.25rem
-    XL: "36px",
-    // XXL: 3.125rem
-    XXL: "50px",
+    XS: "1rem",
+    S: "1.3rem",
+    M: "1.8rem",
+    L: "2.4rem",
+    XL: "3.6rem",
+    XXL: "5rem",
   },
 };
