9aabab69b3decb4cf5ea710a5ac2e3e5c11bcc74
zoodaa <jkj01230@gmail.com>
2023-08-30 23:12:32 +0900

    feat: 해시태그 컴포넌트 구현

---
+++ src/components/pages/main/group-study-page/group-hashtag-box/GroupHashtagBox.js
@@ -0,0 +1,87 @@
+import * as style from "@/components/pages/main/group-study-page/group-hashtag-box/GroupHashtagBox.style";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function GroupHashtagBox() {
+  return (
+    <style.Box>
+      <style.HashtagBox>
+        <Link href="/">
+          <Image
+            src="/assets/img/HashtagBox_고독.svg"
+            alt="HashtagBox_고독"
+            loader={() => "/assets/img/HashtagBox_고독.svg"}
+            width={256}
+            height={66}
+          />
+        </Link>
+        <Link href="/">
+          <Image
+            src="/assets/img/HashtagBox_소통.svg"
+            alt="HashtagBox_소통"
+            loader={() => "/assets/img/HashtagBox_소통.svg"}
+            width={256}
+            height={66}
+          />
+        </Link>
+        <Link href="/">
+          <Image
+            src="/assets/img/HashtagBox_북적.svg"
+            alt="HashtagBox_북적"
+            loader={() => "/assets/img/HashtagBox_북적.svg"}
+            width={256}
+            height={66}
+          />
+        </Link>
+        <Link href="/">
+          <Image
+            src="/assets/img/HashtagBox_한산.svg"
+            alt="HashtagBox_한산"
+            loader={() => "/assets/img/HashtagBox_한산.svg"}
+            width={256}
+            height={66}
+          />
+        </Link>
+      </style.HashtagBox>
+
+      <style.HashtagBox2>
+        <Link href="/">
+          <Image
+            src="/assets/img/HashtagBox_연구.svg"
+            alt="HashtagBox_연구"
+            loader={() => "/assets/img/HashtagBox_연구.svg"}
+            width={256}
+            height={66}
+          />
+        </Link>
+        <Link href="/">
+          <Image
+            src="/assets/img/HashtagBox_빨리.svg"
+            alt="HashtagBox_빨리"
+            loader={() => "/assets/img/HashtagBox_빨리.svg"}
+            width={256}
+            height={66}
+          />
+        </Link>
+        <Link href="/">
+          <Image
+            src="/assets/img/HashtagBox_소심.svg"
+            alt="HashtagBox_소심"
+            loader={() => "/assets/img/HashtagBox_소심.svg"}
+            width={256}
+            height={66}
+          />
+        </Link>
+        <Link href="/">
+          <Image
+            src="/assets/img/HashtagBox_집중.svg"
+            alt="HashtagBox_집중"
+            loader={() => "/assets/img/HashtagBox_집중.svg"}
+            width={256}
+            height={66}
+          />
+        </Link>
+      </style.HashtagBox2>
+    </style.Box>
+  );
+}
---
+++ src/components/pages/main/group-study-page/group-hashtag-box/GroupHashtagBox.style.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Box = styled.div`
+  position: relative;
+  width: 114.1rem;
+  height: 13.2rem;
+  top: 18.9rem;
+  left: 39.3rem;
+`;
+
+export const HashtagBox = styled.div`
+  display: flex;
+  gap: 3.9rem;
+  align-items: center;
+`;
+
+export const HashtagBox2 = styled.div`
+  display: flex;
+  margin-top: 1rem;
+  gap: 3.9rem;
+  align-items: center;
+`;
