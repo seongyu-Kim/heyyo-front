e4dfd44f2f816f41711850c188b3260650199e4f
yoolim <db12fla@naver.com>
2023-09-04 18:04:16 +0900

    feat: 공유자료실 배너 밑 부분 구현

---
+++ src/components/pages/share/share-box/ShareBox.js
@@ -0,0 +1,25 @@
+import React from "react";
+import * as style from "@/components/pages/share/share-box/ShareBox.style";
+import ShareRectangle from "@/components/pages/share/share-rectangle/ShareRectangle";
+import Image from "next/image";
+import ShareSwiper from "@/components/pages/share/share-swiper/ShareSwiper";
+
+export default function ShareBox() {
+    return (
+        <style.ContainerDiv>
+            <style.TitleDiv>
+                <Image
+                    src="/assets/img/title.svg"
+                    alt="Title"
+                    loader={() => "/assets/img/title.svg"}
+                    width={384}
+                    height={47}
+                />
+
+
+            </style.TitleDiv>
+            <ShareSwiper/>
+
+        </style.ContainerDiv>
+    );
+}
---
+++ src/components/pages/share/share-box/ShareBox.style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import { Font } from "@/assets/fonts/Font";
+
+export const ContainerDiv = styled.div`
+  height: 55.5rem;
+  flex-shrink: 0;
+  background-color: #F5F5F5;
+  position:relative;
+`;
+
+
+export const TitleDiv = styled.div`
+  position:relative;
+  color: #000;
+  font-size: ${Font.Size.XL};
+  font-style: normal;
+  top: 8.3rem;
+  left: 80.9rem;
+`;
