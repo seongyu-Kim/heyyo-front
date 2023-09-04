de6251f0dcf765619879f72883a58a2f2e8affcc
yoolim <db12fla@naver.com>
2023-09-04 18:08:31 +0900

    feat: 공유자료실 레이아웃 구현

---
+++ src/components/pages/share/share-layout/ShareLayout.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import ShareBox from "@/components/pages/share/share-box/ShareBox";
+import ShareRectangle from "@/components/pages/share/share-rectangle/ShareRectangle";
+import ShareMiddleBar from "@/components/pages/share/share-middle-bar/ShareMiddleBar";
+import ShareTextBox from "@/components/pages/share/share-text-box/ShareTextBox";
+import { Navigation, Pagination, A11y } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import PaginationDiv from "@/components/pages/share/pagination/Pagination";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import ShareBottomBox from "@/components/pages/share/share-bottom-box/ShareBottomBox";
+import ShareBanner from "@/components/pages/share/share-banner/ShareBanner"; // 추가
+import ShareDiffBottomBox from "@/components/pages/share/share-diff-bottom-box/ShareDiffBottomBox";
+import styled from "styled-components";
+
+const CustomSwiper = styled(Swiper)`
+  .swiper-button-next,
+  .swiper-button-prev {
+    color: gray;
+  }
+`;
+
+export default function ShareLayout() {
+  const [selectedTab, setSelectedTab] = useState("ShareDiffBottomBox"); // 초기 선택 탭 설정
+
+  return (
+    <CustomSwiper
+      navigation
+      pagination={{ clickable: true }}
+      modules={[Navigation, Pagination, A11y]}
+    >
+      <SwiperSlide>
+        <ShareBanner />
+        <ShareBox />
+        <ShareMiddleBar setSelectedTab={setSelectedTab} />
+        {/*<ShareTextBox/>*/}
+        {/*<ShareBottomBox />*/}
+        {selectedTab === "ShareDiffBottomBox" && <ShareDiffBottomBox />}
+        {selectedTab === "ShareBottomBox" && <ShareBottomBox />}
+        {/*<ShareDiffBottomBox />*/}
+        {/*<PaginationDiv />*/}
+      </SwiperSlide>
+    </CustomSwiper>
+  );
+}
