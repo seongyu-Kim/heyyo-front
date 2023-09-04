95f7c491a22239b2426070ef70ede9d3f28ffdc5
yoolim <db12fla@naver.com>
2023-09-04 18:02:18 +0900

    feat: 공유자료실 페이지네이션

---
+++ src/components/pages/share/pagination/Pagination.js
@@ -0,0 +1,15 @@
+import React from "react";
+import * as style from "@/components/pages/share/pagination/Pagination.style";
+
+export default function Pagination() {
+    return (
+        <div>
+            <style.PaginationDiv>1</style.PaginationDiv>
+            <style.PaginationDiv>2</style.PaginationDiv>
+            <style.PaginationDiv>3</style.PaginationDiv>
+            <style.PaginationDiv>4</style.PaginationDiv>
+            <style.PaginationDiv>5</style.PaginationDiv>
+            <style.PaginationDiv>6</style.PaginationDiv>
+        </div>
+    );
+}
---
+++ src/components/pages/share/pagination/Pagination.style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const PaginationDiv = styled.div`
+  width: 0.6798rem;
+  color: black;
+  
+  font-size: 1.7rem;
+  //font-style: normal;
+  //font-weight: 500;
+  //line-height: normal;
+  display: flex;
+  margin-bottom: 5.2rem;
+  right: 2.2rem;
+  //letter-spacing: 2.1rem;
+  position: relative;
+`;
