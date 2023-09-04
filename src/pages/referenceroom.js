d5ce07a9148d38a26f49df5f3f2f2c2795ba479a
yoolim <db12fla@naver.com>
2023-09-04 20:49:35 +0900

    fix: 경로 수정 및 에러 수정

--- src/pages/referenceroom.js
+++ src/pages/referenceroom.js
@@ -1,43 +1,5 @@
-import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
 import ShareLayout from "@/components/pages/share/share-layout/ShareLayout";
-import ShareBanner from "@/components/pages/share/share-banner/ShareBanner";
-import ShareBox from "@/components/pages/share/share-box/ShareBox";
-import ShareMiddleBar from "@/components/pages/share/share-middle-bar/ShareMiddleBar";
-import ShareBottomBox from "@/components/pages/share/share-bottom-box/ShareBottomBox";
-import PaginationDiv from "@/components/pages/share/pagination/Pagination";
 
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import styled from "styled-components";
-import { Navigation, Pagination, A11y } from "swiper/modules";
-
-const CustomSwiper = styled(Swiper)`
-  .swiper-button-next,
-  .swiper-button-prev {
-    color: gray;
-  }
-`;
-
-export default function ReferenceRoom() {
-  return (
-      <ShareLayout>
-        <CustomSwiper
-            navigation
-            pagination={{ clickable: true }}
-            modules={[Navigation, Pagination, A11y]}
-        >
-          <SwiperSlide>
-            <ShareBanner />
-            <ShareBox />
-            <ShareMiddleBar />
-            {/*<ShareTextBox/>*/}
-            <ShareBottomBox />
-            {/*<ShareDiffBottomBox/>*/}
-            <PaginationDiv />
-          </SwiperSlide>
-        </CustomSwiper>
-      </ShareLayout>
-  );
+export default function referenceroom() {
+    return <ShareLayout />;
 }
